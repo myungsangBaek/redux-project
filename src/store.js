fc0a30cfbd2da66d4715af06df3e1bbd0ffb2275
myungsangBaek <bms6168@naver.com>
2022-03-20 18:13:14 +0900

    [FEATURE] Redux toolkit create action

--- src/store.js
+++ src/store.js
@@ -1,28 +1,16 @@
+//Toolkit
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteTodo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+const addToDo = createAction("ADD");
+const deleteTodo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteTodo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
@@ -36,3 +24,44 @@ export const actionCreators = {
 };
 
 export default store;
+
+//General
+
+// import { createStore } from "redux";
+
+// const ADD = "ADD";
+// const DELETE = "DELETE";
+
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
+
+// const deleteTodo = (id) => {
+//   return {
+//     type: DELETE,
+//     id: parseInt(id),
+//   };
+// };
+
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return [{ text: action.text, id: Date.now() }, ...state];
+//     case DELETE:
+//       return state.filter((toDo) => toDo.id !== action.id);
+//     default:
+//       return state;
+//   }
+// };
+
+// const store = createStore(reducer);
+
+// export const actionCreators = {
+//   addToDo,
+//   deleteTodo,
+// };
+
+// export default store;
