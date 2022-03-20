d8d8d908547b4449950f0a02956c83d0fca0ba4f
myungsangBaek <bms6168@naver.com>
2022-03-20 18:28:17 +0900

    [FEATURE] Generate createReducer

--- src/store.js
+++ src/store.js
@@ -1,22 +1,19 @@
 //Toolkit
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteTodo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteTodo]: (state, action) => {
+    state.filter((toDo) => toDo.id !== action.payload);
+  },
+});
+const store = configureStore(reducer);
 
 export const actionCreators = {
   addToDo,
